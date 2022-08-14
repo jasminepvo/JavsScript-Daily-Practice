c013038dfdaad774ec072e098c4485539f4fe8c1
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-14 10:53:59 -0400

    8/14/22 - simple math

---
+++ 8kyu-OppositeNumber.js
@@ -0,0 +1,14 @@
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+FUNDAMENTALS - 8kyu
+*/
+
+function opposite(number) {
+	return (number = -number);
+}
