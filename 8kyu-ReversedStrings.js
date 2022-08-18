80b080120738b55bcadd8bf674411b2be42eb0a6
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-18 12:35:16 -0400

    8/18/22 - split()reverse()join()

---
+++ 8kyu-ReversedStrings.js
@@ -0,0 +1,11 @@
+/*
+DESCRIPTION:
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+STRINGSFUNDAMENTALS
+*/
+
+//Solve time: 30seconds
