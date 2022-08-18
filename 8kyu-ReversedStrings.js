c080a779e16035f5e846cf7a6964a5da0d5c0182
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-18 12:41:17 -0400

    8/18/22 - split()reverse()join()

--- 8kyu-ReversedStrings.js
+++ 8kyu-ReversedStrings.js
@@ -8,4 +8,8 @@ Complete the solution so that it reverses the string passed into it.
 STRINGSFUNDAMENTALS
 */
 
+function solution(str) {
+	return str.split("").reverse().join("");
+}
+
 //Solve time: 30seconds
