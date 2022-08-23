ae179d67447d350f0673df2cca566aa829bd2d7f
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-23 12:50:48 -0400

    8/23/22 - split().join()

---
+++ 8kyu-RemoveStringSpaces.js
@@ -0,0 +1,12 @@
+/*
+DESCRIPTION:
+Simple, remove the spaces from the string, then return the resultant string.
+
+FUNDAMENTALSSTRINGSARRAYS
+*/
+
+function noSpace(x) {
+	return x.split(" ").join("");
+}
+
+//Solve time: 3min
