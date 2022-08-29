6d304de8136f1b62551224f0e7a71725cb9172fe
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-29 12:31:34 -0400

    8/29/22 - String()

---
+++ 8kyu-ConvertBooleanToString.js
@@ -0,0 +1,14 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+STRINGSFUNDAMENTALS
+*/
+
+function booleanToString(b) {
+	return String(b);
+}
+
+//Solve time: 3min21sec
+//I started off overcomplicating it using a ternary operator but I didn't have the return
