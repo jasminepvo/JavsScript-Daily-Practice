c2cff7c32451c62f464921663915e1becf896cb1
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-29 12:36:02 -0400

    8/29/22 - map()

---
+++ 8kyu-LostWithoutAMap.js
@@ -0,0 +1,15 @@
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+FUNDAMENTALSARRAYS
+*/
+
+function maps(x) {
+	return x.map((element) => element * 2);
+}
+
+//Solve time: 30sec
