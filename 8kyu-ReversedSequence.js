55bdee4d5964a5f36149dddce51437236d0fb90f
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-17 11:53:43 -0400

    9/17/22 - array.push(), reverse()

---
+++ 8kyu-ReversedSequence.js
@@ -0,0 +1,17 @@
+/*
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+
+FUNDAMENTALS
+*/
+
+const reverseSeq = (n) => {
+	let arr = [];
+	for (let i = 1; i <= n; i++) {
+		arr.push(i);
+	}
+	return arr.reverse();
+};
+
+//Solve time: 10min
