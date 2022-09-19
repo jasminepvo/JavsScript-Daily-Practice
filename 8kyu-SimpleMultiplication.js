3c4df38e2c3fff82e01068604ad3b2de368e6da2
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-18 20:08:42 -0400

    9/18/22 - ternary operatory

---
+++ 8kyu-SimpleMultiplication.js
@@ -0,0 +1,11 @@
+/*
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+FUNDAMENTALS
+*/
+
+function simpleMultiplication(number) {
+	return number % 2 === 0 ? number * 8 : number * 9;
+}
+
+//Solve time: 20seconds
