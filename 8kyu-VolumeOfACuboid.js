fe7e04f697b47e9a3d7cdb784ce3b7f360f62f7b
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-30 08:25:11 -0400

    9/30/22 - Refactored solution using ES6

---
+++ 8kyu-VolumeOfACuboid.js
@@ -0,0 +1,13 @@
+/*
+Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+GEOMETRYFUNDAMENTALS
+*/
+
+class Kata {
+	static getVolumeOfCuboid(length, width, height) {
+		return length * width * height;
+	}
+}
+
+//Refactored solve time: 20sec
