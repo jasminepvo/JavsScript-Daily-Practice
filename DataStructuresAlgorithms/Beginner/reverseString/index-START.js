a40313e0cec0f33f70b820c42822f281d369c18b
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-10-12 13:11:15 -0400

    For...of way ES6

--- DataStructuresAlgorithms/Beginner/reverseString/index-START.js
+++ DataStructuresAlgorithms/Beginner/reverseString/index-START.js
@@ -6,8 +6,8 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 function reverseString(text) {
 	let result = "";
 
-	for (let i = text.length - 1; i >= 0; i--) {
-		result += text[i];
+	for (let char of text) {
+		result = char + result;
 	}
 	return result;
 }
