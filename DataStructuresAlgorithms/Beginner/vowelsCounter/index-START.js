c7494a6cdbde764a093a1c8c164a464490fc0c31
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-10-12 13:23:09 -0400

    Iterative approach to counting the number of vowels in a string of text

--- DataStructuresAlgorithms/Beginner/vowelsCounter/index-START.js
+++ DataStructuresAlgorithms/Beginner/vowelsCounter/index-START.js
@@ -3,11 +3,18 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
+const vowels = ["a", "e", "i", "o", "u"];
 
 function vowelsCounter(text) {
-    // Code goes here
+	let counter = 0;
+
+	for (let letter of text.toLowerCase()) {
+		if (vowels.includes(letter)) {
+			counter++;
+		}
+	}
+
+	return counter;
 }
-
-
 
 module.exports = vowelsCounter;
