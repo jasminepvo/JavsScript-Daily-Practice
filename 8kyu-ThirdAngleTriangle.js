fd01b2d6281215f782f56b1b0e90c7377bb6892e
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-10-14 14:53:38 -0400

    10/14/22 - Refactored solution using ES6, math

---
+++ 8kyu-ThirdAngleTriangle.js
@@ -0,0 +1,15 @@
+/*
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+https://en.wikipedia.org/wiki/Triangle
+
+FUNDAMENTALS
+*/
+
+const otherAngle = (a, b) => 180 - a - b;
+
+//Refactored solve time: 30sec
