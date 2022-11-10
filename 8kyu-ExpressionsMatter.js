c027a711f29903b6be6d59edd94d5534422dd527
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-11-09 19:56:02 -0500

    11/9/22 - Refactored solution using ES6, Math.max()

--- 8kyu-ExpressionsMatter.js
+++ 8kyu-ExpressionsMatter.js
@@ -21,11 +21,5 @@ You cannot swap the operands. For instance, in the given example you cannot get 
 */
 
 const expressionMatter = (a, b, c) => {
-	let max1 = a * (b + c);
-	let max2 = a * b * c;
-	let max3 = a + b * c;
-	let max4 = (a + b) * c;
-	let max5 = a + b + c;
-
-	return Math.max(max1, max2, max3, max4, max5);
+	return Math.max(a * (b + c), a * b * c, a + b * c, (a + b) * c, a + b + c);
 };
