bb8785ea773a1fb0b315d1383cda61ecc97bf025
Jasmine P. Vo <99847030+jasminepvo@users.noreply.github.com>
2024-11-19 11:10:38 -0600

    easy - hash map (refactored)

--- leetcode/1-TwoSum.js
+++ leetcode/1-TwoSum.js
@@ -1,9 +1,8 @@
 /*
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 You can return the answer in any order.
+Easy - hash map
 */
 
 /**
@@ -11,36 +10,81 @@ You can return the answer in any order.
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    //P - nums array, target (num, integer)
-    //R - indices of the num that add up to the target
-    //E - [3, 2, 4], target is 6.
-    //3+2 = 5 so not this one
-    //3+4 = 7 so not this one
-    //2+4 = 6 so this is the target we are looking for
-    //which would return index 1 and 2
-    //[1,2]
-    
-    //initialize an empty map
-    const map = new Map()
 
-    //loop through the nums array until end of the array length
-    for (let i=0; i<nums.length; i++) {
-        //initialize variable that will be the complement of the target and current index of the array
-        let complement = target - nums[i]
+function twoSum(nums, target) {
+  const map = new Map();
 
-        //check to see if the complement exists in the map
-        if (map.has(complement)) {
-            return [map.get(complement), i]
-        }
+  for (let i=0; i<nums.length; i++) {
+    let complement = target - nums[i]
 
-        //otherwise add the current num and its index to our map
-        map.set(nums[i], i)
+    if (map.has(complement)) {
+        return [map.get(complement), i]
     }
-}
+
+    map.set(nums[i], i)
+  }
+};
 
 //Time: O(n)
 //Space: O(n)
 
-console.log(twoSum([2, 7, 11, 15], 9)) //[0,1]
-console.log(twoSum([3, 2, 4], 6)) //[1,2]
+//Parameters - nums array, target (num, integer)
+//Return - indices of the num that add up to the target
+//Example - 
+//[3, 2, 4], target is 6.
+//3+2 = 5 doesn't equal 6, so not this one
+//3+4 = 7 doesn't equal 6, so not this one
+//2+4 = 6 so this is the target we are looking for
+//which would return index 1 and 2
+//[1,2]
+// Pseduocode -
+//initialize an empty map
+//loop through the nums array until end of the array length
+//initialize variable that will be the complement of the target and current index of the array
+//check to see if the complement exists in the map
+//otherwise add the current num and its index to our map
+
+console.log(twoSum([2, 7, 11, 15], 9)); //[ 0, 1 ]
+/*
+Iteration 0:
+i=0, nums[0]=2
+complement = 9 - 2 = 7
+map = { }
+7 does not exist in map so we set it
+map = { (2, 0) }
+
+Iteration 1:
+i=1, nums[1]=7
+complement = 9 - 7 = 2
+map = { (2, 0) }
+2 does exist in map so we return the index it is at, along with the current index (i)
+map.has(complement) = map.has(2) = index 0
+i = 1
+return [0, 1]
+*/
+
+console.log(twoSum([3, 2, 4], 6)); //[ 1, 2 ]
+/*
+Iteration 0:
+i=0, nums[0]=3
+complement = 6 - 3 = 3
+map = { }
+3 does not exist in map so we set it
+map = { (3, 0) }
+
+Iteration 1:
+i=1, nums[1]=2
+complement = 6 - 2 = 4
+map = { (3, 0) }
+4 does exist in map so we set it
+map = { (3, 0), (2, 1) }
+
+Iteration 2:
+i=2, nums[2]=4
+complement = 6 - 4 = 2
+2 exists in map so we return the index it is at, along with the current index (i)
+map.has(complement) = map.has(2) = index 1
+i = 2
+return [1, 2]
+*/
+
